7a262ebbe0e4a9e4281f41ba33907964bc0877dd
gortron <gordon.lanza@gmail.com>
2020-03-25 17:46:50 -0700

    added logic to only respond with unique posts, and sort posts by params given

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -13,24 +13,45 @@ class ServerController < ApplicationController
     # test if terms are valid
     # fetch all posts for each tag in URL query
     # combine all posts from the fetch, remove duplicates
+    # apply whatever sorts user has requested
+
+    # tags ("science", "technology")
+    # sortBy ("popularity")
+    # order ("asc", "desc") 
     url = request.original_url
     uri = URI.parse(url)
     queries = CGI.parse(uri.query)
     tags = queries["tags"].first.split(",")
-    sortBy = queries["sortBy"].first
-    direction = queries["direction"].first
+    sortBy = queries["sortBy"].first || "id"
+    direction = queries["direction"].first || "asc"
 
     endpoint = 'http://hatchways.io/api/assessment/blog/posts?'
-    posts = [];
+    response = [];
+    uniquePosts = {};
     tags.each do |tag|
       target = endpoint + "tag=" + tag
       data = JSON.parse(RestClient.get(target))
-      posts.push(data["posts"])
+      # look through posts that come back from API call
+      # check if that posts id we've seen before
+      # if it's new, let's note it in uniquePosts & push to response
+      # if it's not, look to the next post (ignore it)
+
+      data["posts"].each do |post|
+        if !uniquePosts.key?(:post["id"]) 
+          uniquePosts[post["id"]] = post
+          # uniquePosts[post["id"]] = true
+        end
+      end
     end
-    # target = endpoint + "tag=" + tags.first
 
+    # build response array from uniqePosts
+    uniquePosts.map { |k,v| response.push(v) }
 
-    message = { "hello": 'posts' }
-    json_response(message)
+    response = response.sort_by { |post| post["#{sortBy}"]}
+    if (direction === "desc") 
+      response = response.reverse
+    end
+
+    json_response(response)
   end
 end
