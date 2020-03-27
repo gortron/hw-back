645f9e026aef04da827c177978b4939623fbf899
gortron <gordon.lanza@gmail.com>
2020-03-27 16:07:59 -0700

    added error handling for cases where no query is provided to the posts route, and where tags, sort_by, and direction are invalid

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -9,49 +9,60 @@ class ServerController < ApplicationController
   # if we wanted to cache our results, we would need to save the final result for any given uri... add a new model, CachedSearches
   # CachedSearches have-many posts. Before external query, we check CachedSearches
   def posts
-    # figure out terms for our fetch
-    # test if terms are valid
-    # fetch all posts for each tag in URL query
-    # combine all posts from the fetch, remove duplicates
-    # apply whatever sorts user has requested
-
-    # tags ("science", "technology")
-    # sortBy ("popularity")
-    # order ("asc", "desc") 
-    url = request.original_url
-    uri = URI.parse(url)
-    queries = CGI.parse(uri.query)
-    tags = queries["tags"].first.split(",")
-    sortBy = queries["sortBy"].first || "id"
+    begin
+      url = request.original_url
+      uri = URI.parse(url)
+      raise NoMethodError unless queries = CGI.parse(uri.query)
+    rescue
+      return json_response({"error": "queries must be provided. Try: /api/posts?tags=tech&sortBy=likes"}, 400)
+    end
+    
+    tags = queries["tags"] || []
+    sort_by = queries["sortBy"].first || "id"
     direction = queries["direction"].first || "asc"
+
+
+    if (tags.empty?)
+      return json_response({"error": "Tags parameter is required"}, 400)
+    else
+      tags = tags.first.split(",")
+    end
+
+    valid_sorts = ["author", "authorId", "id","likes", "popularity", "reads"]
+    if (!valid_sorts.include?(sort_by))
+      return json_response({
+        "error": "sortBy parameter is invalid. Try: id, author, authorId, likes, popularity, reads"
+        }, 400)
+    end
+    
+    valid_directions = ["asc", "desc"]
+    if (!valid_directions.include?(direction))
+      return json_response({
+        "error": "direction parameter is invalid. Try: asc, desc"
+        }, 400)
+    end
 
     endpoint = 'http://hatchways.io/api/assessment/blog/posts?'
     response = [];
-    uniquePosts = {};
+    unique_posts = {};
     tags.each do |tag|
       target = endpoint + "tag=" + tag
       data = JSON.parse(RestClient.get(target))
-      # look through posts that come back from API call
-      # check if that posts id we've seen before
-      # if it's new, let's note it in uniquePosts & push to response
-      # if it's not, look to the next post (ignore it)
 
       data["posts"].each do |post|
-        if !uniquePosts.key?(:post["id"]) 
-          uniquePosts[post["id"]] = post
-          # uniquePosts[post["id"]] = true
+        if !unique_posts.key?(:post["id"]) 
+          unique_posts[post["id"]] = post
         end
       end
     end
 
-    # build response array from uniqePosts
-    uniquePosts.map { |k,v| response.push(v) }
 
-    response = response.sort_by { |post| post["#{sortBy}"]}
+    unique_posts.map { |k,v| response.push(v) }
+    response = response.sort_by { |post| post["#{sort_by}"]}
     if (direction === "desc") 
       response = response.reverse
     end
 
-    json_response(response)
+    return json_response(response)
   end
 end
